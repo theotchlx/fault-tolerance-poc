e02e9a78104e6164318b21e2fe747825b5f3a4d0
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-03-15 13:18:33 +0100

    refactor(ui/main.go): remove unused code

--- ui/main.go
+++ ui/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"html/template"
 	"io"
 	"log"
@@ -13,27 +12,6 @@ type Message struct {
 	ID   int    `json:"id"`
 	User string `json:"user"`
 	Text string `json:"text"`
-}
-
-// Fetch messages from backend
-func fetchMessages() ([]Message, error) {
-	resp, err := http.Get("http://localhost:8081/messages")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var messages []Message
-	if err := json.Unmarshal(body, &messages); err != nil {
-		return nil, err
-	}
-
-	return messages, nil
 }
 
 // Serve frontend
